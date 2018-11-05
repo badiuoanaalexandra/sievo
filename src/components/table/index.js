98f168e9d050303b4f2d9bca804863b8510951a4
Oana Badiu <badiuoanaalexandra@gmail.com>
2018-11-05 23:01:29 +0200

    Play around with react-table and columns

--- src/components/table/index.js
+++ src/components/table/index.js
@@ -9,34 +9,25 @@ import "react-table/react-table.css";
 class ProjectTable extends React.Component {
 
   render() {
-    const data = this.props.filter ? this.props.filteredProjects : this.props.projects;
-    if(data === null) return (
+    if(this.props.projects === null) return (
       <div>fetching data</div>
     )
 
+    if(this.props.projects.length === 0) return (
+      <div>There are no projects</div>
+    )
+
+    const data = this.getData();
     return (
       <div>
           <ReactTable
-          data={data}
+          data={this.getData()}
+          showPagination={false}
+          defaultPageSize={data.length}
           columns={[
             {
               Header: "Projects",
-              columns: [
-                {
-                  Header: "Project",
-                  accessor: "project"
-                },
-                {
-                  Header: "Description",
-                  id: "description",
-                  accessor: d => d.description
-                },
-                {
-                  Header: "Category",
-                  id: "category",
-                  accessor: d => d.category
-                }
-              ]
+              columns: this.getColumns()
             },
           ]}
           defaultPageSize={10}
@@ -44,6 +35,41 @@ class ProjectTable extends React.Component {
         />
       </div>
     );
+  }
+  getData = () => {
+    return this.props.filter ? this.props.filteredProjects : this.props.projects;
+  }
+
+  //we will use the fields of the first project to build our columns
+  //other way to do this is to just add columns "manually", but every time a new column is added to the API we would need to also add it here
+  //to avoid extra work we will just build our columns dynamically
+  //assumption is that if the field name has "Date" it's date type, if it cannot convert it to a date, it just returns the string - other way to do this is probably use some regex
+  getColumns = () => {
+    const referenceProject = this.props.projects[0];
+      let cols = [];
+
+      for (let field in referenceProject){
+        const isDate = field.includes('date');
+        let columnConfig = {
+            Header: field.toUpperCase(),
+            id:field.toLowerCase(),
+            accessor: d => {
+              if (d[field] === "NULL") return ""
+              return field.includes('date') ? new Date(d[field]).toLocaleDateString("fi-FI") : d[field]
+            }
+          }
+
+        cols.push({
+            Header: field.toUpperCase(),
+            id:field.toLowerCase(),
+            accessor: d => {
+              if (d[field] === "NULL") return ""
+              return field.includes('date') ? new Date(d[field]).toLocaleDateString("fi-FI") : d[field]
+            }
+          });
+      }
+
+      return cols;
   }
 }
 
--- src/helpers/projectsFilter.js
+++ src/helpers/projectsFilter.js
@@ -1,3 +1,5 @@
+// we could have used also the react-table filter on the description column
+//when there is a lot of data involved most likely the filtering happens by calling the api, so we could replace this with an api call 
 export const fetchFilteredProjects = async (description, projects) => {
   try{
     const filteredProjects = projects.filter(p => p.description.toLowerCase().includes(description));
